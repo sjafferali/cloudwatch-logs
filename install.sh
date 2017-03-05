5a46aa7212f2bf5a3b60ab485c976684327e1d8d
Samir Jafferali <samirjafferali@gmail.com>
2017-03-05 10:34:14 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,65 +1,67 @@
 #!/bin/bash
 
-REGION="us-east-1"
-CONFIG="https://raw.githubusercontent.com/sjafferali/cloudwatch-logs/master/awslogs.conf"
+if [[ ! -d "/var/awslogs" ]]
+then
+    REGION="us-east-1"
+    CONFIG="https://raw.githubusercontent.com/sjafferali/cloudwatch-logs/master/awslogs.conf"
+    python <(curl -s https://raw.githubusercontent.com/sjafferali/cloudwatch-logs/master/awslogs-agent-setup.py) -r $REGION -c $CONFIG --non-interactive
+fi
 
-python <(curl -s https://raw.githubusercontent.com/sjafferali/cloudwatch-logs/master/awslogs-agent-setup.py) -r $REGION -c $CONFIG --non-interactive
-
-wget -P /var/awslogs/etc/config/ https://raw.githubusercontent.com/sjafferali/cloudwatch-logs/master/config/comon
+wget -O /var/awslogs/etc/config/comon https://raw.githubusercontent.com/sjafferali/cloudwatch-logs/master/config/comon
 
 if [[ -f /var/log/holland/holland.log ]]
 then
-  wget -P /var/awslogs/etc/config/ https://raw.githubusercontent.com/sjafferali/cloudwatch-logs/master/config/holland
+  wget -O /var/awslogs/etc/config/holland https://raw.githubusercontent.com/sjafferali/cloudwatch-logs/master/config/holland
 fi
 
 if [[ -f /var/log/mariadb/mariadb.log ]]
 then
-  wget -P /var/awslogs/etc/config/ https://raw.githubusercontent.com/sjafferali/cloudwatch-logs/master/config/mariadb
+  wget -O /var/awslogs/etc/config/mariadb https://raw.githubusercontent.com/sjafferali/cloudwatch-logs/master/config/mariadb
 fi
 
 if [[ -f /var/log/mysqld.log ]]
 then
-  wget -P /var/awslogs/etc/config/ https://raw.githubusercontent.com/sjafferali/cloudwatch-logs/master/config/mysql
+  wget -O /var/awslogs/etc/config/mysql https://raw.githubusercontent.com/sjafferali/cloudwatch-logs/master/config/mysql
 fi
 
 if [[ -f /var/log/nginx/access.log ]]
 then
-  wget -P /var/awslogs/etc/config/ https://raw.githubusercontent.com/sjafferali/cloudwatch-logs/master/config/nginx
+  wget -O /var/awslogs/etc/config/nginx https://raw.githubusercontent.com/sjafferali/cloudwatch-logs/master/config/nginx
 fi
 
 if [[ -f /var/log/php-fpm/error.log ]]
 then
-  wget -P /var/awslogs/etc/config/ https://raw.githubusercontent.com/sjafferali/cloudwatch-logs/master/config/phpfpm
+  wget -O /var/awslogs/etc/config/phpfpm https://raw.githubusercontent.com/sjafferali/cloudwatch-logs/master/config/phpfpm
 fi
 
 if [[ -f /var/log/redis/redis.log ]]
 then
-  wget -P /var/awslogs/etc/config/ https://raw.githubusercontent.com/sjafferali/cloudwatch-logs/master/config/redis
+  wget -O /var/awslogs/etc/config/redis https://raw.githubusercontent.com/sjafferali/cloudwatch-logs/master/config/redis
 fi
 
 if [[ -f /var/log/gunicorn/access_log ]]
 then
-  wget -P /var/awslogs/etc/config/ https://raw.githubusercontent.com/sjafferali/cloudwatch-logs/master/config/gunicorn
+  wget -O /var/awslogs/etc/config/gunicorn https://raw.githubusercontent.com/sjafferali/cloudwatch-logs/master/config/gunicorn
 fi
 
 if [[ -f /opt/backupdir/logs/fs.log ]]
 then
-  wget -P /var/awslogs/etc/config/ https://raw.githubusercontent.com/sjafferali/cloudwatch-logs/master/config/backup-fs
+  wget -O /var/awslogs/etc/config/backup-fs https://raw.githubusercontent.com/sjafferali/cloudwatch-logs/master/config/backup-fs
 fi
 
 if [[ -f /var/log/httpd/error_log ]]
 then
-  wget -P /var/awslogs/etc/config/ https://raw.githubusercontent.com/sjafferali/cloudwatch-logs/master/config/httpd
+  wget -O /var/awslogs/etc/config/httpd https://raw.githubusercontent.com/sjafferali/cloudwatch-logs/master/config/httpd
 fi
 
 if [[ -f /usr/local/prm/logs/kill.log ]]
 then
-  wget -P /var/awslogs/etc/config/ https://raw.githubusercontent.com/sjafferali/cloudwatch-logs/master/config/prm
+  wget -O /var/awslogs/etc/config/prm https://raw.githubusercontent.com/sjafferali/cloudwatch-logs/master/config/prm
 fi
 
 if [[ -f /opt/sandbox-toolkit/logs/api.log ]]
 then
-  wget -P /var/awslogs/etc/config/ https://raw.githubusercontent.com/sjafferali/cloudwatch-logs/master/config/web-api
+  wget -O /var/awslogs/etc/config/web-api https://raw.githubusercontent.com/sjafferali/cloudwatch-logs/master/config/web-api
 fi
 
 systemctl enable awslogs
